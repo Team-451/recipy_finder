d6637795feab9bde02a940e0cbd662d67982af9c
Medha Narayan <medhanarayanccs@gmail.com>
2023-11-18 22:09:17 +0530

    Commited

---
+++ Desktop/recipefinder/myrecipes/src/components/Header.js
@@ -0,0 +1,20 @@
+// src/components/Header.js
+import React from 'react';
+import './Header.css'; // Import your main CSS file
+import './NavigationLinks.css'; // Import the new CSS file
+import HomeLink from './Home';
+import AboutLink from './Aboutme';
+
+function Header() {
+  return (
+    <header>
+      <h1>RecipeFinder</h1>
+      <nav className="header-links-container">
+        <HomeLink />
+        <AboutLink />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
