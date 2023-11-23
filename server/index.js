ee4eed4b66707e2c129a4cf3c3d2f244f5cdd2d7
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-11-24 00:47:24 +0530

    recipy fix

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const authRoute = require("./Routes/AuthRoute");
+const authRoute = require("./routes/AuthRoute");
 const { MONGODB_URL, PORT } = process.env;
 
 mongoose
@@ -12,7 +12,7 @@ mongoose
   .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log(`Server is listening on port 4000`);
 });
 
