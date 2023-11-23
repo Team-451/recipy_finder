c3588fc5247de1eab9ac3a87a19ea2022cb20b62
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-11-24 01:35:15 +0530

    options method add 2

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -4,4 +4,11 @@ const router = require("express").Router();
 router.post("/signup", Signup);
 router.post("/login", Login);
 
+router.options("/signup", (req, res) => {
+  res.sendStatus(200);
+});
+router.options("/login", (req, res) => {
+  res.sendStatus(200);
+});
+
 module.exports = router;
